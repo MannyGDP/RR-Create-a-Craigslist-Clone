5b5862ca7876f01f766bcdba053169b0c1d1831e
Manny Delgado <mannygdp@gmail.com>
2023-08-16 21:19:23 -0700

    changes

--- src/Components/Item.jsx
+++ src/Components/Item.jsx
@@ -1,5 +1,10 @@
-export default function Item() {
+export default function Item(props) {
     return (
-        <h1>Item</h1>
+        <div style={{border: '1px solid black', borderRadius: '8px', width: '200px', height: '300px', margin: "15px", boxShadow: '0px 0px 5px #000000'}}>
+            <span>{props.price}</span>
+            <div style={{width: '100%', textAlign: 'center'}}>{props.title}</div>
+            <img src={props.imageURL} alt="" />
+            <div style={{width: '100%', textAlign: 'center'}}>{props.description}</div>
+        </div>
     )
 }
